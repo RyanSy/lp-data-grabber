7a839c2cba3bfed0891a2ae1f34a555cbc3e857f
Ryan Sy <ryansy@HP400.mynetworksettings.com>
2024-06-22 21:18:13 -0400

    write to csv now working

--- getNewReleases.js
+++ getNewReleases.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const client_id = process.env.CLIENT_ID; 
 const client_secret = process.env.CLIENT_SECRET;
+const writeCSV = require('write-csv');
 
 /**
  * Get access token
@@ -24,7 +25,7 @@ async function getToken() {
  * Get new releases
  */
 async function getNewReleases(access_token) {
-    const response = await fetch("https://api.spotify.com/v1/browse/new-releases?limit=10", {
+    const response = await fetch("https://api.spotify.com/v1/browse/new-releases?limit=50", {
         method: 'GET',
         headers: { 'Authorization': 'Bearer ' + access_token },
     });
@@ -33,11 +34,14 @@ async function getNewReleases(access_token) {
 }
 
 getToken().then(response => {
-    let token = response.access_token;
+    const token = response.access_token;
 
+    //  get new releases
     getNewReleases(token).then(async data => {
-        let newReleases = data.albums.items;
+        const newReleases = data.albums.items;
+        const products = [];
 
+        //  for each new release, format according to Shopify csv template and save to array
         for (let i = 0; i < newReleases.length; i++) {
             if (newReleases[i].album_type != 'single') {
                 const response = await fetch(newReleases[i].href, {
@@ -45,16 +49,71 @@ getToken().then(response => {
                     headers: { 'Authorization': 'Bearer ' + token },
                 });
                 const album = await response.json();
-                // console.log(album);
-                console.log('artist:', album.artists[0].name);
-                console.log('title:', album.name);
-                console.log('genres:', album.genres);
-                console.log('image:', album.images[0].url);
-                console.log('upc:', album.external_ids.upc);
-                console.log('album type:', album.album_type);
-                console.log('');
+                const artist = album.artists[0].name;
+                const title = `${album.artists[0].name} - ${album.name}`;
+                const genres = album.genres;
+                const image = album.images[0].url;
+                const upc = album.external_ids.upc;
+                const albumType = album.album_type;
+                const product = {
+                    'Handle': '',
+                    'Title': title,
+                    'Body (HTML)': '',
+                    'Vendor': 'Village Record Club',
+                    'Product Category': 'Media > Music & Sound Recordings > Records & LPs',
+                    'Type': albumType,
+                    'Tags': `${artist}, ${genres}`,
+                    'Published': '',
+                    'Option1 Name': '',
+                    'Option1 Value': '',
+                    'Option2 Name': '',
+                    'Option2 Value': '',
+                    'Option3 Name': '',
+                    'Option3 Value': '',
+                    'Variant SKU': upc,
+                    'Variant Grams': '',
+                    'Variant Inventory Tracker': '',
+                    'Variant Inventory Qty': '',
+                    'Variant Inventory Policy': 'deny',
+                    'Variant Fulfillment Service': 'manual',
+                    'Variant Price': '',
+                    'Variant Compare At Price': '',
+                    'Variant Requires Shipping': '',
+                    'Variant Taxable': '',
+                    'Variant Barcode': '',
+                    'Image Src': image,
+                    'Image Position': '',
+                    'Image Alt Text': '',
+                    'Gift Card': '',
+                    'SEO Title': '',
+                    'SEO Description': '',
+                    'Google Shopping / Google Product Category': '',
+                    'Google Shopping / Gender': '',
+                    'Google Shopping / Age Group': '',
+                    'Google Shopping / MPN': '',
+                    'Google Shopping / AdWords Grouping': '',
+                    'Google Shopping / AdWords Labels': '',
+                    'Google Shopping / Condition': '',
+                    'Google Shopping / Custom Product': '',
+                    'Google Shopping / Custom Label 0': '',
+                    'Google Shopping / Custom Label 1': '',
+                    'Google Shopping / Custom Label 2': '',
+                    'Google Shopping / Custom Label 3': '',
+                    'Google Shopping / Custom Label 4': '',
+                    'Variant Image': '',
+                    'Variant Weight Unit': '',
+                    'Variant Tax Code': '',
+                    'Cost per item': '',
+                    'Price / International': '',
+                    'Compare At Price / International': '',
+                    'Status': 'Active',
+                };
+                products.push(product);
             }
         }
+
+        // write to csv - figure out other location and unique file names later
+        writeCSV('./products.csv', products);
     }).catch(err => console.error(err));
 
 }).catch(err => console.error(err));
